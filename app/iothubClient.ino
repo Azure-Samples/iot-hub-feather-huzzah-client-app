61240a48c01daa0e22cf316e7ed5dff243e75f80
Renlong Tu <turenlong@qq.com>
2017-06-12 14:23:47 +0800

    Update code based on the AzureIotHub 1.0.34 (#14)
    * Update code based on the AzureIotHub 1.0.34： Remove AzureIoTHubClient class
    
    * Fix comment style

--- app/iothubClient.ino
+++ app/iothubClient.ino
@@ -8,20 +8,26 @@ const char *notFound = "\"No method found\"";
  * As a temporary solution, we will test the definition of AzureIoTHubVersion, which is only defined
  *    in the new AzureIoTHub library version. Once we totally deprecate the last version, we can take
  *    the #ifdef out.
+ *
+ * AzureIotHub library remove AzureIoTHubClient class in 1.0.34, so we remove the code below to avoid
+ *    compile error
+*/
+
+/*
+ * #ifdef AzureIoTHubVersion
+ * static AzureIoTHubClient iotHubClient;
+ * void initIoThubClient()
+ * {
+ *     iotHubClient.begin(sslClient);
+ * }
+ * #else
+ * static AzureIoTHubClient iotHubClient(sslClient);
+ * void initIoThubClient()
+ * {
+ *     iotHubClient.begin();
+ * }
+ * #endif
  */
-#ifdef AzureIoTHubVersion
-static AzureIoTHubClient iotHubClient;
-void initIoThubClient()
-{
-    iotHubClient.begin(sslClient);
-}
-#else
-static AzureIoTHubClient iotHubClient(sslClient);
-void initIoThubClient()
-{
-    iotHubClient.begin();
-}
-#endif
 
 static void sendCallback(IOTHUB_CLIENT_CONFIRMATION_RESULT result, void *userContextCallback)
 {
